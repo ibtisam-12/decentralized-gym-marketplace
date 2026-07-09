6bb0649fcaf5b33f663b565665984cbbafae16cd
Muhammad Ibtisam <143593855+ibtisam-12@users.noreply.github.com>
2026-07-09 20:13:16 +0500

    fix: auto-generate Product slug from name on save

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -20,6 +21,11 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
